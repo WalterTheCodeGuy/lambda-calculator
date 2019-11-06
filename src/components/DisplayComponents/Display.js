5f76078ff6e1aa273db1f06144d31a1b1d876548
Walter Futch <walter.futch@gmail.com>
2019-11-05 21:39:26 -0500

    MVP Complete
    MVP is complete

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+const Display = (props) => {
+  const [displayState] = useState(0);
+  return <div className="display">{displayState}</div>;
 };
+
+export default Display;
